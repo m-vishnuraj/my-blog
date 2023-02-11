da7e34d8b6c3015d6dd3ecbe23eaa54071cc1e14
m-vishnuraj <mvishnurajm@gmail.com>
2023-02-11 15:15:35 +0530

    completed

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import articles from './ArticleContent'
 
 const ArticlePage = () => {
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
     return (
-        <div>ArticlePage</div>
-    )
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
+            ))}
+        </>
+    );
 }
 
 export default ArticlePage
