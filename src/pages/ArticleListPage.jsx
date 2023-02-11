26d0cd9b468e3b9ef749c31328781d1f377321bb
m-vishnuraj <mvishnurajm@gmail.com>
2023-02-11 15:21:58 +0530

    modular article completed

--- src/pages/ArticleListPage.jsx
+++ src/pages/ArticleListPage.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import ArticleList from '../components/ArticleList'
 import articles from './ArticleContent'
 
 const ArticleListPage = () => {
     return (
         <>
             <h1>Articles</h1>
-            {articles.map(article => (
-                <Link key={article.name} className='article-list-item' to={`/articles/${article.name}`}>
-                    <h3>{article.title}</h3>
-                    <p>{article.content[0].substring(0, 150)}...</p>
-                </Link>
-            ))}
+            <ArticleList articles={articles} />
         </>
     )
 }
